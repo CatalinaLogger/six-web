ce57da1a9f25d279a72c06e21a223ca78fd2abd2
JinPing <1641525429@qq.com>
2019-06-14 10:06:09 +0800

    #

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://10.10.10.165:8888"',
-  WEB_SOCKET: '"ws://10.10.10.165:8888/web/socket"'
+  BASE_API: '"http://oa.bdsmc.net:16488"',
+  WEB_SOCKET: '"ws://oa.bdsmc.net:16488/web/socket"'
 })
